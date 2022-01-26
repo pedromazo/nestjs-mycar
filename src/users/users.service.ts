0cdd3d6af1e417f3043303ba80f185827acc3e88
Pedro Mazo <pedrolmazo@gmail.com>
2022-01-26 11:25:23 -0300

    Users service completed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,4 +13,33 @@ export class UsersService {
             const user = this.repo.create({ email, password});
             return this.repo.save(user)
         }
+
+        findOne(id: number) {
+            const user = this.repo.findOne(id)
+            return user;
+
+        }
+
+        find(email: string) {
+            const users = this.repo.find({ email });
+            return users;
+        }
+
+        async update(id: number, attrs: Partial<User>) {
+            const user = await this.findOne(id);
+            if(!user) {
+                throw new Error('User not found');
+            }
+            Object.assign(user, attrs); //take all properties of attrs and apply to user;
+            return this.repo.save(user);
+
+        }
+
+        async remove(id: number) {
+            const user = await this.findOne(id);
+            if(!user) {
+                throw new Error ('User not found');
+            }
+            return this.repo.remove(user);
+        }
 }
